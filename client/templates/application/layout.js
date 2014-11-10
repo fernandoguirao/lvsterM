645563a0323f3df1c193c25a637ec5f28e70c504
Fernando Guirao <fernando@bueninvento.es>
2014-11-10 21:34:08 +0100

    versión libro completa

--- client/templates/application/layout.js
+++
@@ -1,7 +0,0 @@
-Template.layout.helpers({
-  pageTitle: function() { return Session.get('pageTitle'); }
-});
-
-Tracker.autorun( function() { 
-  Session.set('pageTitle', 'Yet another value');
-} );
